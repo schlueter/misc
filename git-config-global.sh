95722398ae84a4311c619a51a0a36c772852e9d1
Schlueter <b@schlueter.blue>
2017-11-10 12:30:23 +1100

    Convert git config file to sh script and add global git ignore

--- git-config-global.sh
+++ git-config-global.sh
@@ -1,12 +1,26 @@
+#!/bin/sh
 # remember that this needs to be an email address registered with your account on GitHub
-user.email=b@schlueter.blue
+git config --global user.email b@schlueter.blue
+
 # for continuity of history it helps to always use an identical name here
-user.name=Schlueter
+git config --global user.name Schlueter
+
 # I guess this is a default, I don't recall setting it
-color.ui=auto
+git config --global color.ui auto
+
 # this keeps git quiet because it really wants this to be set, despite the fact that it says something about a default
-push.default=simple
+git config --global push.default simple
+
 # setting the commentchar to something other than the default #
 # allows for markdown in pull requests created using hub
 # TODO make vim syntax highlighting respect this
-core.commentchar=%
+git config --global core.commentchar %
+
+# This is one of the default values according to the git-config man page
+#
+# git config --global core.excludesFile ~/.config/git/ignore
+cat >~/.config/git/ignore <<EOF
+*.pyc
+__pycache__/
+.vagrant/
+EOF
