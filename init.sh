42dc0e5ab1be390ed937335102058f30a72b5bd7
Schlueter <b@schlueter.blue>
2017-12-10 00:54:44 -0500

    Comment up main init script

--- init.sh
+++ init.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
-cp .tmux.conf ~
-mkdir -p ~/.config/ ~/workspace
-./install-tmux-from-source.sh
+# Install tmux config
+cp tmux.conf ~/.tmux.conf
+
+# Create some directories
+mkdir ~/.config/ ~/workspace
+
+# Configure git
 source git-config-global.sh
+
+# Install tools from source
+./install-tmux-from-source.sh
 # TODO ./install-hub-from-source.sh
 # TODO ./install-git-from-source.sh
 # TODO ./install-vim-from-source.sh
 # TODO ./install-zsh-from-source.sh
-git clone git@github.com:schlueter/bin ~/bin
+
+# Clone configuration repositories
 git clone git@github.com:schlueter/zsh-config ~/.config/zsh
+git clone git@github.com:schlueter/bin ~/bin
 git clone git@github.com:schlueter/dot-vim ~/.vim
 git clone git@github.com:powerline/fonts ~/workspace/fonts
-~/workspace/fonts/install.sh
-git clone git://github.com/altercation/solarized.git ~/workspace/solarized
+git clone git@github.com:altercation/solarized.git ~/workspace/solarized
 
+# Install fonts
+~/workspace/fonts/install.sh
+
+# Run system init
 ./$(uname -s)-init
