e2dcdaf8ef48131426b93d97aecf32048c1ae73c
Schlueter <b@schlueter.blue>
2017-12-23 14:56:25 -0500

    Merge system specific init scripts into main one

--- init.sh
+++ init.sh
@@ -17,7 +17,8 @@ mkdir ~/.config/ ~/workspace
 # TODO ./install-zsh-from-source.sh
 
 # Clone configuration repositories
-git clone git@github.com:schlueter/zsh-config ~/.config/zsh
+git clone --recursive git@github.com:schlueter/zsh-config ~/.config/zsh
+# TODO ruby-build install
 git clone git@github.com:schlueter/bin ~/bin
 git clone git@github.com:schlueter/dot-vim ~/.vim
 git clone git@github.com:powerline/fonts ~/workspace/fonts
@@ -26,5 +27,58 @@ git clone git@github.com:altercation/solarized.git ~/workspace/solarized
 # Install fonts
 ~/workspace/fonts/install.sh
 
-# Run system init
-./$(uname -s)-init
+case "$(uname -s)" in
+    (Linux)
+        sudo apt update
+        sudo apt install    \
+            alsa-base       \
+            dbus-x11        \
+            feh             \
+            gnome-keyring   \
+            libnotify-bin   \
+            libreadline-dev \
+            libssl-dev      \
+            parted          \
+            pulseaudio      \
+            python-dbus-dev \
+            python-gtk2-dev \
+            python-pip      \
+            upower          \
+            vlc-nox         \
+            xscreensaver    \
+        ;;
+    (Darwin)
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        for cask in \
+            iterm2 \
+            docker \
+            minikube \
+            transmission \
+            vagrant \
+            virtualbox
+        do
+            brew cask install $cask
+        done
+
+        for app in \
+            git \
+            gnupg \
+            openssl \
+            pinentry \
+            pkg-config \
+            transmission \
+            tree \
+            vim \
+            zsh
+        do
+            brew install $app
+        done
+
+        open ~/workspace/solarized/iterm2-colors-solarized/*
+        ;;
+
+esac
+
+# rbenv install 2.4.3
+# pyenv install 2.7.14
+# pyenv install 3.6.3
