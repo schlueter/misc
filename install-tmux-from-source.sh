89c208cf90f76a95391f64bc4bc2ad744f72ecbd
Schlueter <b@schlueter.blue>
2017-12-23 21:27:47 -0500

    Install tmux from source; reorg tmux install

--- install-tmux-from-source.sh
+++ install-tmux-from-source.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-TMUX_VER=2.6
+TMUX_VER=master
 LIBEVENT_VER=2.1.8-stable
 TEMP_COMPILE=~/tmux-temp-compile
 COMMON_INSTALL_PREFIX=/opt
-SYMLINK=/usr/local/bin/tmux
 
 set -e
 ((DEBUG)) && set -x
@@ -17,25 +16,37 @@ cd ${TEMP_COMPILE}
 
 printf "\n>>> Downloading the releases ...\n"
 
-curl -OL https://github.com/tmux/tmux/releases/download/${TMUX_VER}/tmux-${TMUX_VER}.tar.gz
 curl -OL https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VER}/libevent-${LIBEVENT_VER}.tar.gz
 
-printf "\n>>> Extracting tmux %s and libevent %s ...\n" ${TMUX_VER} ${LIBEVENT_VER}
-
-tar xzf tmux-${TMUX_VER}.tar.gz
+printf "\n>>> Extracting libevent %s ...\n" ${TMUX_VER} ${LIBEVENT_VER}
 tar xzf libevent-${LIBEVENT_VER}.tar.gz
 
-printf "\n>>> Compiling libevent ...\n"
-
-cd libevent-${LIBEVENT_VER}
-./configure --prefix=${COMMON_INSTALL_PREFIX}
-sudo make
-sudo make install
-
-printf "\n>>> Compiling tmux ...\n"
-
 (
-    cd ../tmux-${TMUX_VER}
+    printf "\n>>> Compiling libevent ...\n"
+    cd libevent-${LIBEVENT_VER}
+    ./configure --prefix=${COMMON_INSTALL_PREFIX}
+    make
+    sudo make install
+)
+
+if [ "$TMUX_VER" = "master" ]
+then
+    git clone https://github.com/tmux/tmux.git
+    TMUX_BUILD_DIR=tmux
+    (
+        cd "$TMUX_BUILD_DIR"
+        ./autogen.sh
+    )
+else
+    curl -OL https://github.com/tmux/tmux/releases/download/${TMUX_VER}/tmux-${TMUX_VER}.tar.gz
+    printf "\n>>> Extracting tmux %s...\n" ${TMUX_VER} ${LIBEVENT_VER}
+    tar xzf tmux-${TMUX_VER}.tar.gz
+    TMUX_BUILD_DIR=tmux-$TMUX_VER
+fi
+(
+    printf "\n>>> Compiling tmux ...\n"
+    cd $TMUX_BUILD_DIR
+
     LDFLAGS="-L${COMMON_INSTALL_PREFIX}/lib" CPPFLAGS="-I${COMMON_INSTALL_PREFIX}/include" LIBS="-lresolv" ./configure --prefix=${COMMON_INSTALL_PREFIX}
     make
 
@@ -43,11 +54,7 @@ printf "\n>>> Compiling tmux ...\n"
 
     sudo make install
 
-    printf "\n>>> Symlink to it from %s ...\n" ${SYMLINK}
-
-    sudo ln -s ${COMMON_INSTALL_PREFIX}/bin/tmux ${SYMLINK}
-
     printf "\n>>> Cleaning up by removing the temporary dir %s ...\n" ${TEMP_COMPILE}
 ) || exit
 
-sudo rm -rf ${TEMP_COMPILE}
+rm -rf ${TEMP_COMPILE}
