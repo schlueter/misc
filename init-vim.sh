8b3a06e3467f486f6394d872aec6bdae16ca3595
Schlueter <b@schlueter.blue>
2019-08-24 23:58:38 -0700

    Parameterize PYTHON_VERSION in init-vim script

--- init-vim.sh
+++ init-vim.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env zsh
 
+PYTHON_VERSION=3.7.4
+
 echo 'Initializing vim config...' >&2
-git clone git@github.com:schlueter/dot-vim "$HOME/.vim"
+git clone git@github.com:schlueter/dot-vim "$HOME/.vim" >&2
 vim -c 'PlugInstall|qa'
 
 echo 'Setting up environment for YouCompleteMe...' >&2
 PYTHON_CONFIGURE_OPTS=--enable-shared \
-    pyenv install 3.6.5
+    pyenv install "$PYTHON_VERSION"
 
-pyenv shell 3.6.5
+pyenv shell "$PYTHON_VERSION"
 pip install virtualenv
 
 pushd "$HOME/.vim"
