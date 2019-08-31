c8ac1377a0ed4e9441834a0510caab301b57e88f
Schlueter <b@schlueter.blue>
2019-08-30 21:19:18 -0700

    Simplify vim-init, transfering YCM init to .vim config

--- init-vim.sh
+++ init-vim.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env zsh
 
-PYTHON_VERSION=3.7.4
-
 echo 'Initializing vim config...' >&2
 git clone git@github.com:schlueter/dot-vim "$HOME/.vim" >&2
+
+echo 'Installing vim-plug...' >&2
+git clone git@github.com:junegunn/vim-plug /tmp/vim-plug/ >&2
+cp /tmp/vim-plug/plug.vim "$HOME/.vim/autoload"
 vim -c 'PlugInstall|qa'
-
-echo 'Setting up environment for YouCompleteMe...' >&2
-PYTHON_CONFIGURE_OPTS=--enable-shared \
-    pyenv install "$PYTHON_VERSION"
-
-pyenv shell "$PYTHON_VERSION"
-pip install virtualenv
-
-pushd "$HOME/.vim"
-    virtualenv venv
-    source venv/bin/activate
-    pushd plugged/YouCompleteMe
-        ./install.py
-    popd
-popd
